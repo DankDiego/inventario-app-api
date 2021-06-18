54d4d5242121508d0ab527a45874032c6efee574
DankDiego <diegosulcamedina33@gmail.com>
2021-06-18 05:37:37 -0500

    gifs

--- api/utils/multer.js
+++ api/utils/multer.js
@@ -6,7 +6,7 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname)
-    if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+    if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png' && ext !== '.gif') {
       cb(new Error('Formato de imagen no soportado'), false)
       return
     }
